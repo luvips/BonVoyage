12c40cd18c9d4f2d7807fefb62c78016d4738ce7
Hornte3113 <alix.montesinos@gmail.com>
2026-02-23 21:43:36 -0600

    probando la funcion useuser para ver los datos que se pueden mandar a la bse de datos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
+//'use client'
+//import{ useUser } from '@clerk/nextjs'
 function DashboardPage() {
+  //const user  = useUser()
+  //console.log(user) esto es solo para ver como se ven los datos de los usuarios, asis e impirme en consola
     return (
     <div>DashboardPage</div>
     )
