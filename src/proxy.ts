9ebfb5133347b87f3114a37cdd8e9700b7c07a4e
Hornte3113 <alix.montesinos@gmail.com>
2026-02-23 21:22:58 -0600

    actualize la autenticacion de los usuarios

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,25 +1,30 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isTenantRoute = createRouteMatcher(['/organization-selector(.*)', '/orgid/(.*)'])
+const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
 
+const isTenantRoute = createRouteMatcher(['/organization-selector(.*)', '/orgid/(.*)'])
 const isTenantAdminRoute = createRouteMatcher(['/orgId/(.*)/memberships', '/orgId/(.*)/domain'])
 
 export default clerkMiddleware(async (auth, req) => {
-  // Restrict admin routes to users with specific Permissions
+  if (isDashboardRoute(req)) {
+    await auth.protect()
+  }
+
+  // Rutas de tenant y admin que ya tenías configuradas
   if (isTenantAdminRoute(req)) {
     await auth.protect((has) => {
       return has({ permission: 'org:admin:example1' }) || has({ permission: 'org:admin:example2' })
     })
   }
-  // Restrict Organization routes to signed in users
-  if (isTenantRoute(req)) await auth.protect()
+  
+  if (isTenantRoute(req)) {
+    await auth.protect()
+  }
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
