d40aa79994a1d67358867b23d340d2be29d1a7a8
Hornte3113 <alix.montesinos@gmail.com>
2026-02-25 12:04:42 -0600

    porfin se puede ver bien la imagen

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,35 +1,26 @@
 import { SignUp } from '@clerk/nextjs'
-import Image from 'next/image'
-import COVER_IMAGE from '../../../../public/register.jpg'
 
-const colors = {
-  primary:"#060606",
-  background:"#f5f5f5",
-  disabled:"#D9D9D9",
-}
-  
 export default function Page() {
-  return(
-<div className='w-full h-screen flex items-start'>
-  <div className='relative w-1/2 h-full flex flex-col'>
-  <div className='absolute top-[20%] left-[10%] flex flex col'>
-    <h1 className='text-4xl text-white font bold my-4'>Tu proxima aventura te espera</h1>
-    <p className='text-xl text-white font normal'>Regístrate para comenzar tu viaje</p>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div className="flex w-full max-w-4xl mx-auto shadow-lg rounded-2xl overflow-hidden bg-white">
+       
+        <div className="hidden md:flex w-1/2 items-center justify-center bg-gray-200 p-6">
+          <div className="w-full h-full min-h-[500px] rounded-2xl bg-gray-300 flex items-center justify-center text-gray-400 text-sm">
+            <img
+  src="/register_3.png"
+  alt="BonVoyage"
+  className="w-full h-full object-cover rounded-2xl"
+/>
+            
+          </div>
+        </div>
 
-  </div>
-  <Image src={COVER_IMAGE} alt="Cover" className="w-full h-full object-cover" />
-  </div>
-
-  <div className='w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20'>
-  <h1 className='text-xl text-[#060606] font-semibold'>VonBoyage</h1>
-
-  </div>
-  <SignUp />
-
-</div>
-  ) 
-  
+       
+        <div className="w-full md:w-1/2 flex items-center justify-center p-8">
+          <SignUp />
+        </div>
+      </div>
+    </div>
+  )
 }
-
-
-//<SignUp />
